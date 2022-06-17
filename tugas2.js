c62656eba4af998452c847f75cdceb37d68f9491
rianmunawar <rianmunawar0513@gmail.com>
2022-06-17 08:13:38 +0700

    Menambahkan tugas2.js

---
+++ tugas2.js
@@ -0,0 +1,7 @@
+let fullName = "Rian Munawar";
+let age = 21;
+let isSingle = true;
+let hobby = ["Gaming", "Reading", "Coding"];
+let wife = null;
+
+console.log(fullName, age, isSingle, hobby, wife);
